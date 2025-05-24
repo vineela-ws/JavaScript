83f2048b641948e5fb38457fc3b8e4e573009711
vineela-ws <amaranenivineela54@gmail.com>
2025-05-24 23:44:21 +0530

    typeOfConcept

---
+++ typeOfConcept.js
@@ -0,0 +1,34 @@
+let x = 30;
+
+console.log(typeof x);
+
+x="hello";
+console.log(typeof x);
+
+
+x={
+    name:'John',
+}
+console.log(typeof x);
+
+let flag = true;
+console.log(typeof flag);
+
+const arr =[1,2,3,4,5]
+console.log(typeof arr);
+
+const j = null;
+console.log(typeof j);
+
+let y;
+console.log(typeof y);
+
+function print(name){
+    console.log("My name is "+name)
+}
+
+console.log(typeof print);"function"
+
+
+
+
